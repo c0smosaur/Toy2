a6a219858aa17ac9eaedb528a58e6de132d8f479
cosmosaur <sjkang539@gmail.com>
2024-01-29 17:16:03 +0900

    feat: #1 add trip, accommodation, stay
    trip, accommodation, stay 개별 저장 기능

--- src/main/java/controller/InsertController.java
+++ src/main/java/controller/InsertController.java
@@ -2,10 +2,16 @@ package controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import entity.AccommodationEntity;
+import entity.StayEntity;
 import entity.TravelEntity;
+import entity.TripEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import repository.AccommodationMapper;
+import repository.StayMapper;
 import repository.TravelMapper;
+import repository.TripMapper;
 
 @RestController
 @RequestMapping("/create")
@@ -13,6 +19,13 @@ public class InsertController {
 
   @Autowired
   private TravelMapper travelMapper;
+  @Autowired
+  private TripMapper tripMapper;
+  @Autowired
+  private AccommodationMapper accMapper;
+  @Autowired
+  private StayMapper stayMapper;
+
   ObjectMapper objectMapper = new ObjectMapper();
 
   @RequestMapping("/")
@@ -29,18 +42,28 @@ public class InsertController {
     return count;
   }
 
-//  @PostMapping("/trip")
-//  public String insertTrip(){
-//
-//  }
-//
-//  @PostMapping("/accommo")
-//  public String insertAccommo(){
-//
-//  }
-//
-//  @PostMapping("/stay")
-//  public String insertStay(){
-//
-//  }
+  @PostMapping("/trip")
+  public Integer insertTrip(@RequestBody TripEntity trip) throws Exception{
+    objectMapper.registerModule(new JavaTimeModule());
+    System.out.println(trip);
+    int count = tripMapper.insertTrip(trip);
+    return count;
+
+  }
+
+  @PostMapping("/accommo")
+  public int insertAccommo(@RequestBody AccommodationEntity accommo) throws Exception{
+    objectMapper.registerModule(new JavaTimeModule());
+    System.out.println(accommo);
+    int count = accMapper.insertAccommodation(accommo);
+    return count;
+  }
+
+  @PostMapping("/stay")
+  public int insertStay(@RequestBody StayEntity stay){
+    objectMapper.registerModule(new JavaTimeModule());
+    System.out.println(stay);
+    int count = stayMapper.insertStay(stay);
+    return count;
+  }
 }
