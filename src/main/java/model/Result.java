d46b6b8a59b50c7071606f55fa95cb42b2ca9bef
Seo Jean Kang <119040660+c0smosaur@users.noreply.github.com>
2024-01-31 16:18:24 +0900

    add: result class
    상태코드, 상태메시지, 데이터를 담는 클래스

---
+++ src/main/java/model/Result.java
@@ -0,0 +1,17 @@
+package model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonNaming(value= PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class Result<T> {
+    private String resultCode;
+    private String resultMessage;
+    private T data;
+}
