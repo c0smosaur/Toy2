a0755d08798e0a9154662b741cb68721a039e887
cosmosaur <sjkang539@gmail.com>
2024-02-02 15:07:08 +0900

    refactor: removed insert-plural methods
    리스트 받아 입력하는 메서드 삭제

--- src/main/java/controller/InsertController.java
+++ src/main/java/controller/InsertController.java
@@ -27,7 +27,7 @@ public class InsertController {
     return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 
-  // 여정(이동, 단수) 입력
+  // 여정(이동) 입력
   @PostMapping("/trip/{travelId}")
   public ResponseEntity insertTrip(@RequestBody TripRequest request,
                                    @PathVariable(name="travelId") int travelId) throws Exception{
@@ -36,20 +36,7 @@ public class InsertController {
     return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 
-  // 여정(이동, 복수) 입력
-  // request 는 trip_id, travel_id가 0으로 들어가서 db 안에서 AI로 받음
-  @PostMapping("/trip-list/{travelId}")
-  public ResponseEntity insertTripList(@RequestBody List<TripRequest> requests,
-                                       @PathVariable(name="travelId") int travelId) throws Exception{
-    int count = 0;
-    for(TripRequest request:requests){
-      count += service.insertTrip(request, travelId);
-    }
-    Result<Object> response = successBuilder(requests);
-    return ResponseEntity.status(HttpStatus.OK).body(response);
-  }
-
-  // 여정(숙박, 단수) 입력
+  // 여정(숙박) 입력
   @PostMapping("/accommodation/{tripId}")
   public ResponseEntity insertAccommo(@RequestBody AccommodationRequest request,
                            @PathVariable(name="tripId") int tripId) throws Exception{
@@ -58,36 +45,12 @@ public class InsertController {
     return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 
-  // 여정(숙박, 복수) 입력
-  @PostMapping("/accommodation-list/{tripId}")
-  public ResponseEntity insertAccommoList(@RequestBody List<AccommodationRequest> requests,
-                                          @PathVariable(name="tripId") int tripId) throws Exception{
-    int count = 0;
-    for (AccommodationRequest request:requests){
-      count += service.insertAccommodation(request, tripId);
-    }
-    Result<Object> response = successBuilder(requests);
-    return ResponseEntity.status(HttpStatus.OK).body(response);
-  }
-
-  // 여정(체류, 단수) 입력
+  // 여정(체류) 입력
   @PostMapping("/stay/{tripId}")
   public ResponseEntity insertStay(@RequestBody StayRequest request,
                         @PathVariable(name = "tripId") int tripId) throws Exception{
     int count = service.insertStay(request, tripId);
     Result<Object> response = successBuilder(request);
-    return ResponseEntity.status(HttpStatus.OK).body(response);
-  }
-
-  // 여정(체류, 복수) 입력
-  @PostMapping("/stay-list/{tripId}")
-  public ResponseEntity insertStay(@RequestBody List<StayRequest> requests,
-                                   @PathVariable(name = "tripId") int tripId) throws Exception{
-    int count = 0;
-    for (StayRequest request: requests){
-      count+=service.insertStay(request, tripId);
-    }
-    Result<Object> response = successBuilder(requests);
     return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 
